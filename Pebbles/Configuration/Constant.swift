16ab67d9d254857a0c943f6ce9fbf480f99db153
usa4060 <cocoa9805@naver.com>
2022-10-02 23:48:31 +0900

    pebbles 템플릿 설정

---
+++ Pebbles/Configuration/Constant.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+//MARK: - Project 전체에서 공통으로 사용하는 값들
+struct Constant {
+    
+    static let BASE_URL = " "
+}
+
---
+++ Pebbles/Configuration/Secret/Secret.swift
@@ -0,0 +1,7 @@
+
+import Foundation
+
+// MARK: Secret 폴더 안에서 사용하는 파일들은 .gitignore에 추가해주세요!!
+struct Secret {
+    static var USER_JWTTOKEN = ""
+}
