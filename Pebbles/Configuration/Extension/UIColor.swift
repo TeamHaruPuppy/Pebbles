2b1a7027394ff40bc84e24174bb521e1ac815435
usa4060 <cocoa9805@naver.com>
2022-10-03 21:14:26 +0900

    pod init

--- Pebbles/Configuration/Extension/UIColor.swift
+++ Pebbles/Configuration/Extension/UIColor.swift
@@ -21,6 +21,18 @@ extension UIColor {
     
     // MARK: 메인 테마 색 또는 자주 쓰는 색을 정의
     // ex. label.textColor = .mainOrange
-    class var mainOrange: UIColor { UIColor(hex: 0xEE8244) }
-    class var searchFieldBorderColor: UIColor { UIColor(hex: 0xE0E0E0) }
+    class var Black: UIColor { UIColor(hex: 0x494F56) }
+    class var Gray_60: UIColor { UIColor(hex: 0x494F56) }
+    class var Gray_50: UIColor { UIColor(hex: 0x5D687A) }
+    class var Gray_40: UIColor { UIColor(hex: 0x919BAB) }
+    class var Gray_30: UIColor { UIColor(hex: 0xCBD0D7) }
+    class var Gray_20: UIColor { UIColor(hex: 0xE8EAED) }
+    class var Gray_10: UIColor { UIColor(hex: 0xF4F5F7) }
+    class var White: UIColor { UIColor(hex: 0xFFFFFF) }
+    class var alart: UIColor {UIColor(hex: 0xF94A4A) }
+    class var Main_30: UIColor {UIColor(hex: 0x35A3FF) }
+    class var Main_20: UIColor {UIColor(hex: 0x5CB4FF) }
+    class var Main_10: UIColor {UIColor(hex: 0x82C6FF) }
+    
+    
 }
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Pebbles' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'Alamofire'
+  pod 'SnapKit'
+  pod 'Then'
+  pod 'Kingfisher'
+  pod 'KakaoSDKCommon'
+  pod 'KakaoSDKAuth'
+  pod 'KakaoSDKUser'
+  # Pods for Pebbles
+
+end
